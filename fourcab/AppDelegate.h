c5aa4221eda8892a8246a50189fb5ec0e8d09ccd
Cameron Hendrix <cameron@leaptank.org>
2013-01-05 11:31:35 -0500

    Initial Commit

---
+++ fourcab/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fourcab
+//
+//  Created by Cameron Hendrix on 1/5/13.
+//  Copyright (c) 2013 LeapTank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fourcab/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  fourcab
+//
+//  Created by Cameron Hendrix on 1/5/13.
+//  Copyright (c) 2013 LeapTank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
