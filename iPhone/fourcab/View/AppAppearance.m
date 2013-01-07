428a602b08d6493463baace1f8fd7d34fbb39bf1
Cameron Hendrix <cameron@leaptank.org>
2013-01-06 19:31:45 -0500

    Sunday 7:30PM

--- iPhone/fourcab/View/AppAppearance.m
+++ iPhone/fourcab/View/AppAppearance.m
@@ -27,22 +27,22 @@
     [[UIBarButtonItem appearance] setBackButtonBackgroundImage:[backButtonHighlightedImage resizableImageWithCapInsets:backButtonEdgeInsets resizingMode:UIImageResizingModeStretch] forState:UIControlStateHighlighted barMetrics:UIBarMetricsDefault];
     
     UIEdgeInsets barButtonItemEdgeInsets = UIEdgeInsetsMake(0.f, 8.f, 0.f, 8.f);
-    UIImage *barButtonItemNormalImage = [UIImage imageNamed:@"barButtonItem-normal.png"];
+    UIImage *barButtonItemNormalImage = [UIImage imageNamed:@"cancelBarButtonItem-normal.png"];
     [[UIBarButtonItem appearance] setBackgroundImage:[barButtonItemNormalImage resizableImageWithCapInsets:barButtonItemEdgeInsets resizingMode:UIImageResizingModeStretch] forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
     
-    UIImage *barButtonItemHighlightedImage = [UIImage imageNamed:@"barButtonItem-highlighted.png"];
+    UIImage *barButtonItemHighlightedImage = [UIImage imageNamed:@"cancelBarButtonItem-highlighted.png"];
     [[UIBarButtonItem appearance] setBackgroundImage:[barButtonItemHighlightedImage resizableImageWithCapInsets:barButtonItemEdgeInsets resizingMode:UIImageResizingModeStretch] forState:UIControlStateHighlighted barMetrics:UIBarMetricsDefault];
         
     NSDictionary *barButtonItemTextAttributes = @{
         UITextAttributeFont : [UIFont fontWithName:@"Noteworthy-Bold" size:13.0f],
         UITextAttributeTextColor : [UIColor whiteColor],
-        UITextAttributeTextShadowColor : [UIColor blackColor],
+        //UITextAttributeTextShadowColor : [UIColor lightGrayColor],
     };
     [[UIBarButtonItem appearance] setTitleTextAttributes:barButtonItemTextAttributes forState:UIControlStateNormal];
     [[UIBarButtonItem appearance] setTitleTextAttributes:barButtonItemTextAttributes forState:UIControlStateHighlighted];
     
     [[UIBarButtonItem appearance] setTitlePositionAdjustment:UIOffsetMake(0.f, 1.0f) forBarMetrics:UIBarMetricsDefault];
-    [[UIBarButtonItem appearance] setBackButtonTitlePositionAdjustment:UIOffsetMake(0.f, 1.0f) forBarMetrics:UIBarMetricsDefault];
+    [[UIBarButtonItem appearance] setBackButtonTitlePositionAdjustment:UIOffsetMake(0.f, -1.0f) forBarMetrics:UIBarMetricsDefault];
      
     // Search Bar
     UIImage *searchBarImage = [UIImage imageNamed:@"searchBarBackground.png"];
